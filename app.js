1c7da91be908ed05330c5de1b923bbf426d7a2d7
Xxavier Polk <xxavier@xxavierpolk.com>
2024-12-16 15:34:00 -0500

    HTML/CSS V1, JS in progress

---
+++ app.js
@@ -0,0 +1,38 @@
+// First up we got the KEYPAD ITEMS
+// const num1 = document.getElementsByClassName('num1')
+// const num2 = document.getElementsByClassName('num2')
+// const num3 = document.getElementsByClassName('num3')
+// const num4 = document.getElementsByClassName('num4')
+// const num5 = document.getElementsByClassName('num5')
+// const num6 = document.getElementsByClassName('num6')
+// const num7 = document.getElementsByClassName('num7')
+// const num8 = document.getElementsByClassName('num8')
+// const num9 = document.getElementsByClassName('num9')
+// const num0 = document.getElementsByClassName('num0')
+// const numstar = document.getElementsByClassName('numstar')
+// const numtag = document.getElementsByClassName('numtag')
+
+// const cancelBtn = document.getElementById('cancel-btn')
+// const clearBtn = document.getElementById('clear-btn')
+// const enterBtn = document.getElementById('enter-btn')
+
+// Everything shown on the SCREEN
+// let display = document.querySelector('.output1')
+// let numPad = document.querySelector('.numonly')
+// function addtoScreen(info) {
+//     display.append(info)
+// }
+
+// num1.addEventListener('click', () => {})
+
+// function clearScreen(){
+
+// }
+
+function btnPress()
+
+
+// Deposit Function
+
+
+// Withdraw Function
